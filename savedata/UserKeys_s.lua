876e4e7989b57f7c3f5215c2bfbbafc46af3e083
Bruno <brunog1kg@gmail.com>
2020-12-29 00:57:38 +0000

    first commit

---
+++ savedata/UserKeys_s.lua
@@ -0,0 +1,12 @@
+USERKEY_1 = {
+	[9] = { EXE = "Hotkey 2-1", KEY1 = 49 },
+	[10] = { EXE = "Hotkey 2-2", KEY1 = 50 },
+	[11] = { EXE = "Hotkey 2-3", KEY1 = 51 },
+	[12] = { EXE = "Hotkey 2-4", KEY1 = 52 },
+	[13] = { EXE = "Hotkey 2-5", KEY1 = 53 },
+	[14] = { EXE = "Hotkey 2-6", KEY1 = 54 },
+	[15] = { EXE = "Hotkey 2-7", KEY1 = 55 },
+	[16] = { EXE = "Hotkey 2-8", KEY1 = 56 },
+	[17] = { EXE = "Hotkey 2-9", KEY1 = 57 },
+}
+
